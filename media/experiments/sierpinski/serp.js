29edafbfb7a2d02e264880ac6ceb9972ccc9640e
Stephen Merity <smerity@smerity.com>
2011-12-15 02:12:12 +1100

    Adding partially completed blog posts on GSS and Mandelbug

--- media/experiments/sierpinski/serp.js
+++ media/experiments/sierpinski/serp.js
@@ -8,36 +8,38 @@
   N = cvs.width;
 
   serp = function(x, y, dist, depth, maxDepth) {
-    var d2, fd2;
-    if (depth == null) depth = 0;
+    var d2;
     if (depth > maxDepth) return;
-    fd2 = dist / 2;
-    d2 = Math.round(fd2);
+    d2 = dist / 2;
     context.beginPath();
     context.moveTo(x, y);
     context.lineTo(x + dist, y + dist);
     context.lineTo(x - dist, y + dist);
     context.closePath();
     context.stroke();
-    serp(x, y, fd2, depth + 1, maxDepth);
-    serp(x + d2, y + d2, fd2, depth + 1, maxDepth);
-    return serp(x - d2, y + d2, fd2, depth + 1, maxDepth);
+    serp(x, y, d2, depth + 1, maxDepth);
+    serp(x + d2, y + d2, d2, depth + 1, maxDepth);
+    return serp(x - d2, y + d2, d2, depth + 1, maxDepth);
   };
 
-  plot = function(mD) {
-    var maxDepth;
-    return serp(N / 2, 0, N / 2, 0, maxDepth = mD);
+  plot = function(maxDepth) {
+    return serp(N / 2, 0, N / 2, 0, maxDepth);
   };
 
   render = function(depth) {
-    var f, newDepth;
+    var f, newDepth, trigger;
     context.clearRect(0, 0, N, N);
     plot(depth);
-    newDepth = depth < 7 ? depth + 1 : 0;
+    trigger = 7;
+    newDepth = depth < trigger ? depth + 1 : 0;
     f = function() {
       return render(newDepth);
     };
-    return setTimeout(f, 400);
+    if (depth === trigger) {
+      return setTimeout(f, 1600);
+    } else {
+      return setTimeout(f, 400);
+    }
   };
 
   render(0);
